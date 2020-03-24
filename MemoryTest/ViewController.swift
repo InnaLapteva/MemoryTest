8956f09925099c9a770653e3b8ec332c9d7db022
InnaL <innalapteva72@gmail.com>
2020-03-24 15:00:59 +0500

    first test

--- MemoryTest/ViewController.swift
+++ MemoryTest/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let person = "Alexey"
+        let age = 39
+        
+        label.text = "Name: \(person), age: \(age)"
+        
+        var first: MyClass? = MyClass()
+        var second = first
+        var third = first
+        
+        first = nil
+        second = nil
+        third = nil
+        
+        var parent: Parent?
+        parent = Parent()
+        parent = nil
     }
-
-
 }
 
+class MyClass {
+    var someNumber = 16
+    
+    deinit {
+        print("class MyClass has been deolocated")
+    }
+}
+//удаление объектов из памяти
+
+class Parent {
+    var child = Child()
+    deinit {
+        print("class Parent has been deolocated")
+    }
+}
+class Child {
+    deinit {
+        print("class Child has been deolocated")
+    }
+}
